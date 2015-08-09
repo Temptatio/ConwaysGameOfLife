0de0997504c31bff67cc590dfc6e0e5ab98e1cd7
Baumschullehrer <baumschullehrer@web.de>
2015-08-09 20:35:42 +0200

    Umformatierung und Endlosschleife eingebaut

--- de.gameoflife/src/de/gameoflife/MainClass.java
+++ de.gameoflife/src/de/gameoflife/MainClass.java
@@ -6,29 +6,31 @@ import javax.swing.JComponent;
 import javax.swing.JFrame;
 
 public class MainClass extends JComponent {
-	
-	private static final int XDIM = 150;
-	private static final int YDIM = 150;
-	private static final int GENERATIONEN = 1000;
-	public static GameOfLife gameOfLife;
-	
-	public static void main(String[] args) throws InterruptedException {	
+
+	private static final int	XDIM			= 150;
+	private static final int	YDIM			= 150;
+	private static final int	GENERATIONEN	= -1;
+	public static GameOfLife	gameOfLife;
+
+	public static void main(String[] args) throws InterruptedException {
 		MainClass fenster = new MainClass();
-		gameOfLife = new GameOfLife(XDIM, YDIM, XDIM*YDIM/3);
-		
+		gameOfLife = new GameOfLife(XDIM, YDIM, XDIM * YDIM / 3);
+
 		JFrame frame = new JFrame("MainClass");
 		frame.add(fenster);
-		frame.setSize(XDIM*4, YDIM*4);
+		frame.setSize(XDIM * 4, YDIM * 4);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		
-		for(int i = 0; i < GENERATIONEN; ++i) {
-		gameOfLife.naechsteGeneration();
-		Thread.sleep(100);
-		fenster.repaint();
+
+		int i = GENERATIONEN;
+		while(i != 0) {
+			gameOfLife.naechsteGeneration();
+			Thread.sleep(100);
+			fenster.repaint();
+			i -= (GENERATIONEN > -1) ? 1 : 0;
+		}
 	}
-	}
-	
+
 	@Override
 	public void paint(Graphics g) {
 		g.drawImage(gameOfLife.GitterAlsBild(), 0, 0, this);
